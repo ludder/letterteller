2d4334c73e8c8d6fddee10f40eabb4244160cbb6
tomgreuter <104893746+tomgreuter@users.noreply.github.com>
2023-10-01 19:30:36 +0200

    Add count comparing

--- app/page.js
+++ app/page.js
@@ -2,16 +2,38 @@
 
 import { useState, useEffect } from 'react';
 
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+const LetterWithCount = ({ letter, count, markColor = true }) => {
+  const color = markColor
+    ? count > 0 ? 'text-green-900'
+      : count === 0 ? 'text-orange-400'
+        : 'text-red-700'
+    : 'text-blue-800'
+
+  return (
+    <span key={letter} className='inline-block px-3 font-mono w-20'>
+      <span className='font-bold inline-block pr-3'>
+        {letter.toUpperCase()}
+      </span>
+      <span className={`italic font-bold ${color}`}>
+        {count}
+      </span>
+    </span>
+  )
+}
+
 export default function Home() {
   const [text, setText] = useState('plak hier de invoertekst');
   const [countedLetters, setCountedLetters] = useState();
+  const [newText, setNewText] = useState('type hier de nieuwe tekst');
+  const [countedNewLetters, setCountedNewLetters] = useState();
 
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
   useEffect
     (
       () => {
-        const countLetters = () => {
+        const countLetters = (text) => {
           const letters = alphabet.split('').reduce((acc, letter) => {
             acc[letter] = 0;
             return acc;
@@ -27,51 +49,77 @@ export default function Home() {
           return letters;
         }
 
-        setCountedLetters(countLetters());
+        setCountedLetters(countLetters(text));
+        setCountedNewLetters(countLetters(newText));
+
+
       },
-      [text]
+      [text, newText]
     );
 
   return (
-    <div className='p-8'>
-
-    <div className="grid grid-cols-2 gap-4 border">
+    <div className='p-8 '>
 
       <h1 className='text-4xl text-blue-800 mb-5'>Letterteller</h1>
-      
-      <main className='flex flex-col gap-4'>
-        <textarea onChange={({ target }) => {
-          setText(target.value);
-        }}
-          className='border border-blue-400 w-96 h-24 p-2'
-          value={text}
-        />
 
-        <p className='text-sm italic'>
-          {text.length} karakters
-        </p>
+      <div className="grid grid-cols-2 gap-4 border">
 
-        {/* <button type="button" onClick={countLetters} className='border w-48 p-4'>Tel de letters</button> */}
+        <div className='flex flex-col gap-4'>
+          <textarea onChange={({ target }) => {
+            setText(target.value);
+          }}
+            className='border border-blue-400 w-96 h-24 p-2'
+            value={text}
+          />
 
-        <div>
-          {
-            countedLetters && Object.entries(countedLetters).map(([letter, count]) => (
-              <span key={letter} className='text-sm inline-block px-2 font-mono'>
-                <span className='font-bold inline-block pr-1'>
-                  {letter.toUpperCase()}
-                </span>
-                <span className='italic'>
-                  {count}
-                </span>
-              </span>
-            ))
-          }
+          <p className='text-sm italic'>
+            {text.length} karakters
+          </p>
 
+          <div>
+            {
+              countedLetters && Object.entries(countedLetters).map(([letter, count]) => (
+                <LetterWithCount
+                  key={letter} letter={letter} count={count}
+                  markColor={false}
+                />
+              ))
+            }
+
+          </div>
         </div>
-      </main>
 
-    </div>
-    
+        <div className='flex flex-col gap-4'>
+          <textarea onChange={({ target }) => {
+            setNewText(target.value);
+          }}
+            className='border border-blue-400 w-96 h-24 p-2'
+            value={newText}
+          />
+
+          <p className='text-sm italic'>
+            {newText.length} karakters
+          </p>
+
+          <div>
+            <p>
+              Te gebruiken:
+            </p>
+
+            {
+              countedNewLetters && Object.entries(countedNewLetters).map(([letter, count]) => {
+                const leftOverCount = (countedLetters[letter] || 0) - count;
+                return (
+                  <LetterWithCount key={letter} letter={letter} count={leftOverCount} />
+                )
+              })
+            }
+
+          </div>
+        </div>
+
+      </div>
+
     </div>
   )
 }
